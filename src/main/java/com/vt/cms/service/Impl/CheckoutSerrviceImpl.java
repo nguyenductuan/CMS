ec3a71a38daaec3ae622a90fd3971a11cf1a04dc
nguyenductuan <nguyenductuanbk1234@gmail.com>
2026-08-28 12:45:19 +0700

    getCheckout with validations and BigDecimal usage
    Added validation for request and items in getCheckout method. Updated total price calculations to use BigDecimal for accuracy.

--- src/main/java/com/vt/cms/service/Impl/CheckoutSerrviceImpl.java
+++ src/main/java/com/vt/cms/service/Impl/CheckoutSerrviceImpl.java
@@ -25,9 +25,18 @@ public class CheckoutSerrviceImpl implements CheckoutService {
 
     @Override
     public CheckoutPreviewResponse getCheckout(CheckoutPreviewRequest request) {
-        //Khởi tạo item
+     // Validate request
+    if (request == null) {
+        throw new RuntimeException("Request không được null");
+    }
+
+    // Validate items
+    if (request.getItems() == null || request.getItems().isEmpty()) {
+        throw new RuntimeException("Danh sách sản phẩm không được để trống");
+    }    
+        //Khởi tạo item 
         List<ItemDto> itemDtos = new ArrayList<>();
-        double totalPrice = 0;
+        BigDecimal totalPrice = 0;
         //       1. Xử lý từng imtem trong request
         for (ItemRequest itemRequest : request.getItems()) {
             ProductResponse product = productRepository.detailProduct(itemRequest.getProductId());
@@ -36,9 +45,14 @@ public class CheckoutSerrviceImpl implements CheckoutService {
             if (product == null) {
                 throw new RuntimeException("Sản phẩm không tồn tại");
             }
+            //Check khi số lượng <= 0
+            if(itemRequest.getQuantity() <=0){
+                 throw new BusinessException("Số lượng phải lớn hơn 0");
+            }
 //                    2.2. Check tồn kho
+            
             if (itemRequest.getQuantity() > product.getStock()) {
-                throw new RuntimeException("Sản phẩm hết hàng");
+                throw new RuntimeException("Sản phẩm không đủ số lượng tồn kho");
             }
             // mapping sang DTO
             ItemDto itemDTO = new ItemDto();
@@ -48,8 +62,8 @@ public class CheckoutSerrviceImpl implements CheckoutService {
             itemDTO.setQuantity(itemRequest.getQuantity());
             itemDTO.setStock(product.getStock());
             // tính tổng tiền đơn hàng  totalPrice
-            double subtotal = product.getPrice() * itemRequest.getQuantity();
-            totalPrice += subtotal;
+           BigDecimal subtotal = product.getPrice() .multiply(BigDecimal.valueOf(itemRequest.getQuantity())); // fix lại cách tính giá
+            totalPrice = totalPrice.add(subtotal); // fix lại cách tính giá
 
             itemDtos.add(itemDTO);
         }
@@ -64,13 +78,12 @@ public class CheckoutSerrviceImpl implements CheckoutService {
             dto.setName(s.getServicename());
             dto.setEstimatedDelivery("2-3 ngày");
             dto.setOriginalFee(s.getFee());
-
             shippingDTOList.add(dto);
         }
         // 5. Chọn shipping mặc định (ví dụ: cái đầu tiên)
-        int shippingFee = shippingDTOList.isEmpty() ? 0 : shippingDTOList.get(0).getOriginalFee();
+       BigDecimal shippingFee = shippingDTOList.isEmpty() ? 0 : shippingDTOList.get(0).getOriginalFee();
         // 6. Tổng tiền = tiền đơn hàng + phí ship
-        double total = totalPrice + shippingFee;
+        BigDecimal total = totalPrice.add(shippingFee);
         // build response
         CheckoutPreviewResponse response = new CheckoutPreviewResponse();
         response.setItems(itemDtos);
