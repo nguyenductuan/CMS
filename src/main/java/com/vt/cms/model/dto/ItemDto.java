1f3e0009fc848083a43023fc4f2f8d8f39ec3184
Nguyen Duc Tuan <nguyenductunabk1234@gmail.com>
2026-08-31 14:07:19 +0700

    fix lại hàm checkout

--- src/main/java/com/vt/cms/model/dto/ItemDto.java
+++ src/main/java/com/vt/cms/model/dto/ItemDto.java
@@ -3,13 +3,15 @@ package com.vt.cms.model.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class ItemDto {
     @JsonProperty("product_id")
     private int productId;
     @JsonProperty("product_name")
     private String productName;
-    private int price;
+    private BigDecimal price;
     private int quantity;
     private int stock;
 
--- src/main/java/com/vt/cms/service/Impl/PriceServiceImpl.java
+++ src/main/java/com/vt/cms/service/Impl/PriceServiceImpl.java
@@ -2,9 +2,10 @@ package com.vt.cms.service.Impl;
 
 import com.vt.cms.model.resp.ProductResponse;
 import com.vt.cms.service.PriceService;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 import java.math.BigDecimal;
-
+@Server
 public class PriceServiceImpl implements PriceService {
     @Override
     public BigDecimal calculateItemPrice(ProductResponse product, int quantity) {
